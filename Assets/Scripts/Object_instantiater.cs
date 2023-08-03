7d93fd48c26f4a4f63978dc57b0c8c194f49207a
Venkata Sai Sandeep M <venkata.ssm@holosuit.com>
2023-08-03 19:33:55 +0530

    Movement done

--- Assets/Scripts/Object_instantiater.cs
+++ Assets/Scripts/Object_instantiater.cs
@@ -9,6 +9,7 @@ public class Object_instantiater : MonoBehaviour
     private GameObject good_egg, poison_egg;
     [SerializeField]
     private int good_egg_count, poison_egg_count;
+    public SnakeController snake_controller;
     private GameObject prefabToInstantiate;
     public Vector3 minBounds;
     public Vector3 maxBounds;
@@ -27,51 +28,91 @@ public class Object_instantiater : MonoBehaviour
         
     }
 
+    //public void instantiateObject()
+    //{
+    //    if(good_egg_count <= 10 && poison_egg_count <= 5)
+    //    {
+    //        int randomIndex = Random.Range(0, 3);
+    //        if (randomIndex == 0)
+    //        {
+    //            prefabToInstantiate = poison_egg;
+    //            poison_egg_count++;
+    //        }
+    //        else
+    //        {
+    //            prefabToInstantiate = good_egg;
+    //            good_egg_count++;
+    //        }
+    //    }
+    //    else if(poison_egg_count > 5)
+    //    {
+    //        prefabToInstantiate = good_egg;
+    //        good_egg_count++;
+    //    }
+    //    else if(good_egg_count>10)
+    //    {
+                
+    //    }
+
+
+    //    Vector3 randomPosition = new Vector3(
+    //            Random.Range(minBounds.x, maxBounds.x),
+    //            Random.Range(minBounds.y, maxBounds.y),
+    //            Random.Range(minBounds.z, maxBounds.z)
+    //        );
+
+    //    Instantiate(prefabToInstantiate, randomPosition, Quaternion.identity);
+    //    if(prefabToInstantiate.tag == "Poison_egg")
+    //    {
+    //        StartCoroutine(destroyGreenEgg(5f));
+    //    }
+    //    //prefabToInstantiate.transform.SetParent();
+    //}
+
     public void instantiateObject()
     {
-        if(good_egg_count <= 10 && poison_egg_count <= 5)
+        if(poison_egg_count >= 4 && good_egg_count >= 9)
         {
-            int randomIndex = Random.Range(0, 3);
-            if (randomIndex == 0)
-            {
-                prefabToInstantiate = poison_egg;
-                poison_egg_count++;
-            }
-            else
-            {
-                prefabToInstantiate = good_egg;
-                good_egg_count++;
-            }
+            snake_controller.gameCompleted();
+            return;
         }
-        else if(poison_egg_count > 5)
-        {
-            prefabToInstantiate = good_egg;
-            good_egg_count++;
-        }
-        else if(good_egg_count>10)
-        {
-            //Game completed sucessfully.
-        }
-        
-
+        int randomIndex = Random.Range(0, 2);
         Vector3 randomPosition = new Vector3(
                 Random.Range(minBounds.x, maxBounds.x),
                 Random.Range(minBounds.y, maxBounds.y),
                 Random.Range(minBounds.z, maxBounds.z)
             );
-
-        Instantiate(prefabToInstantiate, randomPosition, Quaternion.identity);
-        if(prefabToInstantiate.tag == "Poison_egg")
+        if (randomIndex == 0 && poison_egg_count < 5)
         {
-            StartCoroutine(destroyGreenEgg(5f));
-        }
-        //prefabToInstantiate.transform.SetParent();
-    }
 
-    IEnumerator destroyGreenEgg(float x)
+
+            prefabToInstantiate = Instantiate(poison_egg, randomPosition, Quaternion.identity);
+            poison_egg_count++;
+            if (prefabToInstantiate.tag == "Poison_egg")
+            {
+                StartCoroutine(destroyEgg(5f));
+            }
+
+        }
+        else
+        {
+            prefabToInstantiate = good_egg;
+            if(good_egg_count<10)
+            {
+                Instantiate(good_egg, randomPosition, Quaternion.identity);
+                good_egg_count++;
+            }
+            else
+            {
+                instantiateObject();
+            }
+        }
+        
+    }
+    IEnumerator destroyEgg(float x)
     {
         yield return new WaitForSeconds(x);
-        Destroy(prefabToInstantiate);
+        DestroyImmediate(prefabToInstantiate,true);
         instantiateObject();
         
     }
