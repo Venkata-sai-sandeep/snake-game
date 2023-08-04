99564e090a417130ade966ef4b795cc35e41309d
Venkata Sai Sandeep M <venkata.ssm@holosuit.com>
2023-08-04 14:50:59 +0530

    Final commit

--- Assets/Scripts/Object_instantiater.cs
+++ Assets/Scripts/Object_instantiater.cs
@@ -14,61 +14,12 @@ public class Object_instantiater : MonoBehaviour
     private GameObject prefabToInstantiate;
     public Vector3 minBounds;
     public Vector3 maxBounds;
-    //private GameObject snake;
     public void Start()
     {
         good_egg_count = 0;
         poison_egg_count = 0;
-        //snake = transform.GetChild(0).transform.gameObject;
-        instantiateObject();
+        //instantiateObject();
     }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
-    //public void instantiateObject()
-    //{
-    //    if(good_egg_count <= 10 && poison_egg_count <= 5)
-    //    {
-    //        int randomIndex = Random.Range(0, 3);
-    //        if (randomIndex == 0)
-    //        {
-    //            prefabToInstantiate = poison_egg;
-    //            poison_egg_count++;
-    //        }
-    //        else
-    //        {
-    //            prefabToInstantiate = good_egg;
-    //            good_egg_count++;
-    //        }
-    //    }
-    //    else if(poison_egg_count > 5)
-    //    {
-    //        prefabToInstantiate = good_egg;
-    //        good_egg_count++;
-    //    }
-    //    else if(good_egg_count>10)
-    //    {
-                
-    //    }
-
-
-    //    Vector3 randomPosition = new Vector3(
-    //            Random.Range(minBounds.x, maxBounds.x),
-    //            Random.Range(minBounds.y, maxBounds.y),
-    //            Random.Range(minBounds.z, maxBounds.z)
-    //        );
-
-    //    Instantiate(prefabToInstantiate, randomPosition, Quaternion.identity);
-    //    if(prefabToInstantiate.tag == "Poison_egg")
-    //    {
-    //        StartCoroutine(destroyGreenEgg(5f));
-    //    }
-    //    //prefabToInstantiate.transform.SetParent();
-    //}
 
     public void instantiateObject()
     {
--- Assets/Scripts/SnakeController.cs
+++ Assets/Scripts/SnakeController.cs
@@ -36,67 +36,86 @@ public class SnakeController : MonoBehaviour {
 
     void Update() 
     {
+       
         if(!levelSelection.text.Equals("Select Level"))
         {
-            levelSelectionCanvas.SetActive(false);
-            parentObject.GetComponent<Object_instantiater>().enabled = true;
-            if (levelSelection.text.Equals("Easy"))
+            if(!canStart)
             {
-                snake_speed = 2f;
-                Debug.Log(Gap.ToString());
+                levelSelectionCanvas.SetActive(false);
+                endCanavas.SetActive(true);
+                endCanavas.transform.GetChild(0).GetComponent<TextMeshProUGUI>().text = "Press the space button to start the game.";
+                endCanavas.transform.GetChild(1).gameObject.SetActive(false);
+                if (Input.GetKeyDown(KeyCode.Space))
+                {
+                    endCanavas.transform.GetChild(1).gameObject.SetActive(true);
+                    endCanavas.SetActive(false);
+                    canStart = true;
+                    object_instantiater.instantiateObject();
+                }
             }
-            else if (levelSelection.text.Equals("Medium"))
+            
+            else
             {
-                snake_speed = 4f;
-                Gap = 10;
-                Debug.Log(Gap.ToString());
-            }   
-            else if (levelSelection.text.Equals("Hard"))
-            {
-                snake_speed = 6f;
-                Gap = 7;
-                Debug.Log(Gap.ToString());
+                parentObject.GetComponent<Object_instantiater>().enabled = true;
+                if (levelSelection.text.Equals("Easy"))
+                {
+                    snake_speed = 2f;
+                    Debug.Log(Gap.ToString());
+                }
+                else if (levelSelection.text.Equals("Medium"))
+                {
+                    snake_speed = 4f;
+                    Gap = 10;
+                    Debug.Log(Gap.ToString());
+                }
+                else if (levelSelection.text.Equals("Hard"))
+                {
+                    snake_speed = 6f;
+                    Gap = 7;
+                    Debug.Log(Gap.ToString());
+                }
+
+                if (player_score < 0)
+                {
+                    gameCompleted();
+                }
+                if (!gameOver)
+                {
+                    if (Input.GetKeyDown(KeyCode.W) && moveDirection != Vector3.back)
+                    {
+                        moveDirection = Vector3.forward;
+                    }
+                    else if (Input.GetKeyDown(KeyCode.S) && moveDirection != Vector3.forward)
+                    {
+                        moveDirection = Vector3.back;
+                    }
+                    else if (Input.GetKeyDown(KeyCode.A) && moveDirection != Vector3.right)
+                    {
+                        moveDirection = Vector3.left;
+                    }
+                    else if (Input.GetKeyDown(KeyCode.D) && moveDirection != Vector3.left)
+                    {
+                        moveDirection = Vector3.right;
+                    }
+                    transform.position += moveDirection * snake_speed * Time.deltaTime;
+
+                    bodyPartsHistory.Insert(0, transform.position);
+
+
+                    int index = 0;
+                    foreach (var body in snake_body)
+                    {
+                        Vector3 points = bodyPartsHistory[Mathf.Clamp(index * Gap, 0, bodyPartsHistory.Count - 1)];
+                        Vector3 moveDirection = points - body.transform.position;
+                        body.transform.position += moveDirection * snake_speed * Time.deltaTime;
+                        body.transform.LookAt(points);
+                        index++;
+                    }
+
+
+                }
             }
-               
-            if (player_score < 0)
-            {
-                gameCompleted();
-            }
-            if (!gameOver)
-            {
-                if (Input.GetKeyDown(KeyCode.W) && moveDirection != Vector3.back)
-                {
-                    moveDirection = Vector3.forward;
-                }
-                else if (Input.GetKeyDown(KeyCode.S) && moveDirection != Vector3.forward)
-                {
-                    moveDirection = Vector3.back;
-                }
-                else if (Input.GetKeyDown(KeyCode.A) && moveDirection != Vector3.right)
-                {
-                    moveDirection = Vector3.left;
-                }
-                else if (Input.GetKeyDown(KeyCode.D) && moveDirection != Vector3.left)
-                {
-                    moveDirection = Vector3.right;
-                }
-                transform.position += moveDirection * snake_speed * Time.deltaTime;
-
-                bodyPartsHistory.Insert(0, transform.position);
-
-
-                int index = 0;
-                foreach (var body in snake_body)
-                {
-                    Vector3 point = bodyPartsHistory[Mathf.Clamp(index * Gap, 0, bodyPartsHistory.Count - 1)];
-                    Vector3 moveDirection = point - body.transform.position;
-                    body.transform.position += moveDirection * snake_speed * Time.deltaTime;
-                    body.transform.LookAt(point);
-                    index++;
-                }
-
-
-            }
+            
         }
         
     }
@@ -132,7 +151,6 @@ public class SnakeController : MonoBehaviour {
             }
             else gameCompleted();
                 
-            //object_instantiater.instantiateObject();
         }
         else if (other.tag.Equals("Border"))
         {
@@ -142,21 +160,6 @@ public class SnakeController : MonoBehaviour {
     public void onClickRestart()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().name);
-        //Transform parentTransform = transform.parent;
-        //GameObject parentGameobject = parentTransform.gameObject;
-        //parentGameobject.GetComponent<Object_instantiater>().enabled = false;
-        //parentGameobject.GetComponent<Object_instantiater>().enabled = true;
-        //GameObject[] good_eggs = GameObject.FindGameObjectsWithTag("Good_egg");
-        //GameObject[] poison_eggs = GameObject.FindGameObjectsWithTag("Poison_egg");
-        //GameObject[] snake = GameObject.FindGameObjectsWithTag("Snake");
-        //foreach (GameObject go in good_eggs)
-        //    Destroy(go);
-        //foreach (GameObject go in poison_eggs)
-        //    Destroy(go);
-        //foreach (GameObject go in snake)
-        //    Destroy(go);
-        //Start();
-        //object_instantiater.Start();
     }
 
     public void gameCompleted()
